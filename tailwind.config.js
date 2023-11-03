fb3475f636415be891139bc128b8af27c0b34caa
Suminona06 <azizmfadli.04@gmail.com>
2023-11-03 21:48:31 +0700

    modif2.0

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,9 +8,10 @@ module.exports = {
     },
     extend: {
       colors: {
-        primary: '#443C68',
+        primary: '#800000',
         dark: '#0f172a',
         secondary: '#64748b',
+        prime: '#cc0000',
       },
       screens: {
         '2xl' : '1300px',
