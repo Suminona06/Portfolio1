08dd6ae6dedfedbbe5f68d9090ceed74a4c3d587
Suminona06 <azizmfadli.04@gmail.com>
2023-11-07 22:05:54 +0700

    updateFItur

--- script.js
+++ script.js
@@ -2,11 +2,16 @@
 window.onscroll = function() {
     const header = document.querySelector('header');
     const fixedMap = header.offsetTop
+    const toTop = document.querySelector('#to-top');
 
     if(window.pageYOffset > fixedMap) {
         header.classList.add('navbar-fixed');
+        toTop.classList.remove('hidden');
+        toTop.classList.add('flex');
     } else {
         header.classList.remove('navbar-fixed');
+        toTop.classList.remove('flex');
+        toTop.classList.add('hidden');
     }
 }
 
@@ -18,3 +23,12 @@ hamburger.addEventListener('click', function(){
     hamburger.classList.toggle('hamburger-active')
     navMenu.classList.toggle('hidden');
 });
+
+
+// Klik diluar Hamburger
+window.addEventListener('click', function(e) {
+    if (e.target != hamburger && e.target != navMenu) {
+        hamburger.classList.remove('hamburger-active')
+        navMenu.classList.add('hidden');
+    }
+} )
